c2febcffe58d709f59bfa6a4cc099a7150c236f5
Duane17 <duanegadama@gmail.com>
2024-06-23 11:35:25 +0200

    Country hooks commit

---
+++ country-hook/src/components/Country.jsx
@@ -0,0 +1,26 @@
+const Country = ({ country }) => {
+  if (!country) {
+    return null;
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    );
+  }
+
+  const { name, capital, population, flags } = country.data;
+
+  return (
+    <div>
+      <h3>{name.common}</h3> {/* Accessing the common name */}
+      <div>capital {capital}</div>
+      <div>population {population}</div>
+      <img src={flags.png} height="100" alt={`flag of ${name.common}`} /> {/* Accessing the flag URL */}
+    </div>
+  );
+};
+
+export default Country;
